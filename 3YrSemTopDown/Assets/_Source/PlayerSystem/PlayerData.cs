e554ac5b2b89f4cdd038baa9958d913933654c97
Atym-git <chuvachelaad@gmail.com>
2025-09-15 20:13:50 +0300

    fix player shooting

--- 3YrSemTopDown/Assets/_Source/PlayerSystem/PlayerData.cs
+++ 3YrSemTopDown/Assets/_Source/PlayerSystem/PlayerData.cs
@@ -4,10 +4,25 @@ public class PlayerData : MonoBehaviour
 {
     [field: SerializeField]
     public float MoveSpeed { get; private set; } = 5f;
+    
+    [field: SerializeField]
+    public int ClipSize { get; private set; } = 15;
+    
+    [field: SerializeField]
+    public int BulletsAmount { get; private set; } = 150;
+
+    [field: SerializeField]
+    public float BulletSpeed { get; private set; } = 5f;
+
+    [field: SerializeField]
+    public float BulletLifeTime { get; private set; } = 3.5f;
 
     [field: SerializeField]
     public Rigidbody2D Rb2D { get; private set; }
 
     [field: SerializeField]
     public GameObject BulletPrefab { get; private set; }
+
+    [field: SerializeField]
+    public Transform AimTransform { get; private set; }
 }
